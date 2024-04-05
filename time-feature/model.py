c89d45ceb3ecb7d67b0627a44404de81d53cdeaa
LanluZ <lanluz@qq.com>
2024-04-05 18:00:16 +0800

    修复 命名错误

--- time-feature/model.py
+++ time-feature/model.py
@@ -5,11 +5,11 @@ class LSTM(nn.Module):
     # 初始化
     def __init__(self, input_size, hidden_size, num_layers, output_size):
         super(LSTM, self).__init__()
-        self.lstm = nn.LSTM(self.input_size, self.hidden_size, self.num_layers, batch_first=True, bidirectional=False)
-        self.classifier = nn.Sequential(nn.Linear(hidden_size * 2, output_size))
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, bidirectional=False)
+        self.classifier = nn.Sequential(nn.Linear(hidden_size, output_size))
 
     # 前向传播
     def forward(self, x):
         x, _ = self.lstm(x, None)
-        x = self.calssifier(x)
+        x = self.classifier(x)
         return x
